b3c502c594b2544f504b4ad2dae15e5f227804fa
Ryan Tuck <ryntck@gmail.com>
2024-05-20 10:42:42 +0000

    first turn

--- game.py
+++ game.py
@@ -46,16 +46,25 @@ def move_piece(board: Board, army: str, piece: int, loc_i: tuple, loc_f: tuple) 
 
     row_f, col_f = loc_f
     territory_end = territory(board, row_f, col_f)
-    assert territory_end == None # TODO or occupied by us or enemy
+    #assert territory_end == None # TODO or occupied by us or enemy
 
     # set new t_i
     t_i = list(territory_start)
     pc_idx = t_i.index(piece)
     t_i.pop(pc_idx)
-    board.grid[row_i][col_i] = tuple(t_i)
+    if set(t_i) == set([army]):
+        board.grid[row_i][col_i] = None
+    else:
+        board.grid[row_i][col_i] = tuple(t_i)
 
     # set new t_f
-    t_f = (army, piece) # free space
+    if territory_end is None: # free space
+        t_f = (army, piece)
+    elif territory_end[0] == army: # friendly
+        t_f = tuple(list(territory_end) + [piece])
+    else: # attack, TODO implement
+        raise Exception('Attacking not implemented')
+
     board.grid[row_f][col_f] = t_f
 
     return board
@@ -69,9 +78,35 @@ def main():
     board = place_initial_pieces(board)
     print(json.dumps(board.dict()))
 
-    board = move_piece(board, 'a', 1, (0,1), (1,1))
-    print(json.dumps(board.dict()))
+    # turn 1 - a - cost=12, bring 3s out and one scout
+    mvs = [
+        ('a', 1, (0,1),(1,1)), # down
+        ('a', 1, (1,1),(1,0)), # then left
+        ('a', 3, (0,0),(1,0)), # then 3 out
+
+
+        ('a', 1, (0,2),(1,2)), # down
+        ('a', 1, (1,2),(1,3)), # then right
+        ('a', 1, (1,3),(1,4)), # then right again
+        ('a', 3, (0,4),(1,4)), # then 3 out
+        
+        ('a', 1, (0,2),(1,2)), # down
+    ]
+
+    for mv in mvs:
+        board = move_piece(board, *mv)
+        print(json.dumps(board.dict()))
     
 
 if __name__ == '__main__':
     main()
+
+
+# def choose_moves(board, army):
+#     units = 12
+#     for territory in territories(board, army):
+#         for option in available_moves(board, army, territory):
+#             if option.move_type == 'attack':
+#                 board = attack(board, army, option.army, option.territory, option.target)
+#                 units -= option.cost_units
+#                 continue
