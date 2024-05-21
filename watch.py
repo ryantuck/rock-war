d4e835516f427b114b726bbcfce791da562598dd
Ryan Tuck <ryntck@gmail.com>
2024-05-21 10:17:52 +0000

    tabulate

--- watch.py
+++ watch.py
@@ -1,24 +1,27 @@
 import json
 import sys
 
+from tabulate import tabulate
+
 if __name__ == '__main__':
+
     turns = 0
+
     for line in sys.stdin:
+
         print(f'Board: {turns}')
         board = json.loads(line)
-        for row in board['grid']:
-            col_str = ''
-            for col in row:
-                col_str += ' '
-                if col:
-                    col_str += ''.join((str(c) for c in col))
-                else:
-                    col_str += '---'
-                col_str += ' '
-            print(col_str)
+
+        str_grid = [
+            [
+                ''.join(str(c) for c in col) if col else None
+                for col in row
+            ]
+            for row in board['grid']
+        ]
+
+        print(tabulate(str_grid))
 
         turns += 1
         print('\n\n')
-
-
                 
